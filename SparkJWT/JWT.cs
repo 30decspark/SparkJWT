e4358af03bf1bf40bb56d1ff931f945939a1712e
30decspark <30decspark@gmail.com>
2023-07-19 13:39:40 +0700

    publish @1.0.0

---
+++ SparkJWT/JWT.cs
@@ -0,0 +1,84 @@
+﻿namespace SparkJWT
+{
+    public class JWT
+    {
+        private string? token;
+
+        public JWT(){ }
+
+        public JWT(string? token)
+        {
+            this.token = token;
+        }
+
+        public string Token()
+        {
+            string token = Guid.NewGuid().ToString("N");
+            SessionStore.Add(token, (DateTime.Now, new Dictionary<string, object>()));
+            if (this.token == null) this.token = token;
+            return token;
+        }
+
+        public bool CheckAndUpdate()
+        {
+            if (token == null) return false;
+            (DateTime, Dictionary<string, object>) session;
+            SessionStore.TryGetValue(token, out session);
+            if (session == (null, null)) return false;
+            if (session.Item1 < DateTime.Now.AddMinutes(-JWT.ExpiryTimeOut))
+            {
+                SessionStore.Remove(token);
+                SessionStore.TrimExcess();
+                return false;
+            }
+            session.Item1 = DateTime.Now;
+            SessionStore[token] = session;
+            return true;
+        }
+
+        public void Delete()
+        {
+            if (token == null) return;
+            SessionStore.Remove(token);
+            SessionStore.TrimExcess();
+        }
+
+        public void ClearExpires()
+        {
+            foreach (string token in SessionStore.Keys)
+            {
+                (DateTime, Dictionary<string, object>) session;
+                SessionStore.TryGetValue(token, out session);
+                if (session == (null, null)) continue;
+                if (session.Item1 < DateTime.Now.AddMinutes(-JWT.ExpiryTimeOut)) SessionStore.Remove(token);
+            }
+            SessionStore.TrimExcess();
+        }
+
+        public Dictionary<string, object> GetObject()
+        {
+            if (token == null) return new Dictionary<string, object>();
+            (DateTime, Dictionary<string, object>) session;
+            SessionStore.TryGetValue(token, out session);
+            if (session == (null, null)) return new Dictionary<string, object>();
+            if (session.Item1 < DateTime.Now.AddMinutes(-JWT.ExpiryTimeOut)) return new Dictionary<string, object>();
+            session.Item1 = DateTime.Now;
+            return session.Item2;
+        }
+
+        public void SetObject(Dictionary<string, object> dic)
+        {
+            if (token == null) return;
+            (DateTime, Dictionary<string, object>) session;
+            SessionStore.TryGetValue(token, out session);
+            if (session == (null, null)) return;
+            if (session.Item1 < DateTime.Now.AddMinutes(-JWT.ExpiryTimeOut)) return;
+            session.Item1 = DateTime.Now;
+            session.Item2 = dic;
+            SessionStore[token] = session;
+        }
+
+        public static int ExpiryTimeOut { get; set; } = 20; //minute
+        private static Dictionary<string, (DateTime, Dictionary<string, object>)> SessionStore = new Dictionary<string, (DateTime, Dictionary<string, object>)>();
+    }
+}
---
+++ SparkJWT/JWTSession.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkJWT
+{
+    public class JWTSession
+    {
+        private string? token;
+        private Dictionary<string, object> dic = new Dictionary<string, object>();
+
+        public JWTSession(string? token)
+        {
+            if (token == null) return;
+            this.token = token;
+            JWT jwt = new JWT(token);
+            dic = jwt.GetObject();
+        }
+
+        public string? GetString(string key)
+        {
+            if (token == null) return null;
+            object? value;
+            dic.TryGetValue(key, out value);
+            if (value == null) return null;
+            return (string)value;
+        }
+
+        public int? GetInt32(string key)
+        {
+            if (token == null) return null;
+            object? value;
+            dic.TryGetValue(key, out value);
+            if (value == null) return null;
+            return (int)value;
+        }
+
+        public decimal? GetDecimal(string key)
+        {
+            if (token == null) return null;
+            object? value;
+            dic.TryGetValue(key, out value);
+            if (value == null) return null;
+            return (decimal)value;
+        }
+
+        public bool? GetBoolean(string key)
+        {
+            if (token == null) return null;
+            object? value;
+            dic.TryGetValue(key, out value);
+            if (value == null) return null;
+            return (bool)value;
+        }
+
+        public object? GetObject(string key)
+        {
+            if (token == null) return null;
+            object? value;
+            dic.TryGetValue(key, out value);
+            if (value == null) return null;
+            return value;
+        }
+
+        public void SetString(string key, string value)
+        {
+            if (token == null) return;
+            dic[key] = value;
+        }
+
+        public void SetInt32(string key, int value)
+        {
+            if (token == null) return;
+            dic[key] = value;
+        }
+
+        public void SetDecimal(string key, decimal value)
+        {
+            if (token == null) return;
+            dic[key] = value;
+        }
+
+        public void SetBoolean(string key, bool value)
+        {
+            if (token == null) return;
+            dic[key] = value;
+        }
+
+        public void SetObject(string key, object value)
+        {
+            if (token == null) return;
+            dic[key] = value;
+        }
+
+        public void Commit()
+        {
+            if (token == null) return;
+            JWT jwt = new JWT(token);
+            jwt.SetObject(dic);
+        }
+    }
+}
